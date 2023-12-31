581b95e8e336f9c089996af72b2a488afced4b8b
Zhenghan Hu <victoriahuhh@gmail.com>
2023-12-31 22:37:48 +0000

    add task manager feature

--- src/App.js
+++ src/App.js
@@ -95,4 +95,79 @@ export function EditProfile() {
   );
 }
 
+function NewTask({ newTask, handleChange, handleSubmit }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        placeholder="New task"
+        value={newTask.title || ""}
+        onChange={handleChange}
+      />
+      {!newTask.title ? null : (
+        <>
+          <textarea
+            name="description"
+            placeholder="Details..."
+            value={newTask.description || ""}
+            onChange={handleChange}
+          />
+          <button type="submit">Add Task</button>
+        </>
+      )}
+    </form>
+  );
+}
+
+function TasksList({ allTasks, handleDelete }) {
+  return (
+    <ul>
+      {allTasks.map(({ title, description, id }) => (
+        <li key={id}>
+          <div>
+            <h2>{title}</h2>
+            <button onClick={() => handleDelete(id)}>X</button>
+          </div>
+          {!description ? <p>undefined</p> : <p>{description}</p>}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export function AppFunction() {
+  const [newTask, setNewTask] = useState({});
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
+  };
+
+  const [allTasks, setAllTasks] = useState([]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newTask.title) return;
+    setAllTasks((prev) => [newTask, ...prev]);
+    setNewTask({});
+  };
+  const handleDelete = (taskIdToRemove) => {
+    setAllTasks((prev) => prev.filter(
+      (task) => task.id !== taskIdToRemove
+    ));
+  };
+
+  return (
+    <main>
+      <h1>Tasks</h1>
+      <NewTask
+        newTask={newTask}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+    </main>
+  );
+}
+
+
+
 export default ObjectState;
