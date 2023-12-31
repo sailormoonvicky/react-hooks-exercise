2a74b7ac84e20f3277a08d98d897c88808f03366
Zhenghan Hu <victoriahuhh@gmail.com>
2023-12-31 21:59:12 +0000

    add profile form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import React, { useState } from "react";
 
-function App() {
+function ObjectState() {
 
   const [formState, setFormState] = useState({});
 
@@ -39,8 +39,60 @@ function App() {
       </form>
     </div>
   )
-
-
 }
 
-export default App;
+export function EditProfile() {
+  const [profile, setProfile] = useState({});
+
+  const handleChange = ({ target }) => {
+    const {name, value} = target;
+    // const name=target.name
+    // const value=target.value
+
+    setProfile(prevProfile => ({
+      ...prevProfile,
+      [name]:value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(JSON.stringify(profile, '', 2));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={profile.firstName || ''}
+        name="firstName"
+        type="text"
+        placeholder="First Name"
+        onChange = {handleChange}
+      />
+      <input
+        value={profile.lastName || ''}
+        type="text"
+        name="lastName"
+        placeholder="Last Name"
+        onChange = {handleChange}
+      />
+      <input
+        value={profile.bday || ''}
+        type="date"
+        name="bday"
+        onChange = {handleChange}
+      />
+      <input
+        value={profile.password || ''}
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange = {handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+
+  );
+}
+
+export default ObjectState;
